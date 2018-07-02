fe9668fb02940ab60dff63341d50b8ade789b5c7
KumaresanGanesan <kutty025@gmail.com>
2018-07-02 10:13:22 +0000

    added header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./components/header/header";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello rajesh</h1>
+      <Header name="vivek" />
+      <h1>Hello KumaresaN</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
